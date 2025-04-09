3eba5874b9915aa7a48f31fc78e24f023cbc5f2e
JulianoLopesM <monteirojjuliano@gmail.com>
2025-04-08 21:58:44 -0300

    Tipos de dados primitivos e seus métodos

--- src/TiposDeDados/DadosNaoPrimitivos.java
+++ src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,6 +2,24 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
+           /*
+           String, array, Class, enum
+           Objetivo: Criar um ninja, e atribuir métodos a ele.
+
+            */
+        String nome = "Naruto Uzumaki";
+        String nomeUpperCase = nome.toUpperCase();
+        System.out.println(nomeUpperCase);
+
+        String aldeia = "Aldeia da Folha";
+        String aldeiaLowerCase = aldeia.toLowerCase();
+        System.out.println("Aldeia da Folha");
+
+        String nome2 = "Sasuke";
+        System.out.println(nome2.length());
+        String nome2Concat = nome2.concat(" Uchiha");
+        System.out.println(nome2Concat);
+        System.out.println(nome2Concat.length());
 
     }
 }
--- src/TiposDeDados/TipoDados.java
+++ src/TiposDeDados/TipoDados.java
@@ -3,7 +3,6 @@ package TiposDeDados;
 public class TipoDados {
     public static void main(String[] args) {
         int idade = 16;
-        String nome = "naruto";
         long numero = 132323L;
         char inicial = 'n';
         boolean vivoOuMorto = true;
