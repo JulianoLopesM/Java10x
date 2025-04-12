ca0099899ecfb7e253229fce4e0459de3a93faf5
JulianoLopesM <monteirojjuliano@gmail.com>
2025-04-12 20:05:49 -0300

    Array em java

---
+++ src/NivelBasico/Array.java
@@ -0,0 +1,16 @@
+package NivelBasico;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[3];
+        //int[] idade = new int[3];
+        ninja[0] = "Naruto Uzumaki";
+        ninja[1] = "Sasuke Uchiha";
+        ninja[2] = "Sakura Haruno";
+        System.out.println(Arrays.toString(ninja));
+
+    }
+}
