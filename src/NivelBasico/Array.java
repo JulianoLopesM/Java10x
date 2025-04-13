90c2ea0579eb63a1b15410135d272d444162f2a6
JulianoLopesM <monteirojjuliano@gmail.com>
2025-04-13 03:43:20 -0300

    arrays e suas inicializações

--- src/NivelBasico/Array.java
+++ src/NivelBasico/Array.java
@@ -5,12 +5,21 @@ import java.util.Arrays;
 public class Array {
     public static void main(String[] args) {
 
+        //inicializa como null
         String[] ninja = new String[3];
-        //int[] idade = new int[3];
         ninja[0] = "Naruto Uzumaki";
         ninja[1] = "Sasuke Uchiha";
         ninja[2] = "Sakura Haruno";
         System.out.println(Arrays.toString(ninja));
+        //inicializa como 0
+        int[] idade = new int[3];
+        System.out.println(idade[0]);
+        //inicializa como 0.0
+        double[] flutuante = new double[3];
+        System.out.println(flutuante[0]);
+        //inicializa como falso
+        boolean[] verdadeiroOuFalso = new boolean[3];
+        System.out.println(verdadeiroOuFalso[0]);
 
     }
 }
