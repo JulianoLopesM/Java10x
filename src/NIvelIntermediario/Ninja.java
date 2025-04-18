0a0aae3945ebf52e4160234143f19722e3ddfe46
JulianoLopesM <monteirojjuliano@gmail.com>
2025-04-18 03:37:39 -0300

    Introdução a Classes e objetos

---
+++ src/NIvelIntermediario/Ninja.java
@@ -0,0 +1,7 @@
+package NIvelIntermediario;
+
+public class Ninja {
+    String nome;
+    int idade;
+    String Aldeia;
+}
---
+++ src/NIvelIntermediario/main.java
@@ -0,0 +1,24 @@
+package NIvelIntermediario;
+
+public class main {
+    public static void main(String[] args) {
+        Ninja Naruto = new Ninja();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.idade = 16;
+        Naruto.Aldeia = "Aldeia da folha";
+
+        //Criar  ninja Sasuke Uchiha - Sasuke é um OBJETO
+
+        Ninja Sasuke = new Ninja();
+
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.idade = 16;
+        Sasuke.Aldeia = "Aldeia da folha";
+
+        Ninja Sakura = new Ninja();
+        Sakura.nome = "Sakura Haruno";
+        Sakura.idade = 17;
+        Sakura.Aldeia = "Aldeia da folha";
+    }
+
+}
